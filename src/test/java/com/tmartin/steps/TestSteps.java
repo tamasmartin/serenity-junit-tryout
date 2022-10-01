2ecdf782e13bfab737de81e735f1de4cfbaf9ad3
Tamas Martin <tamas.martin@gmail.com>
2022-10-01 17:35:20 -0400

    step added

--- src/test/java/com/tmartin/steps/TestSteps.java
+++ src/test/java/com/tmartin/steps/TestSteps.java
@@ -1,11 +1,15 @@
 package com.tmartin.steps;
 
+import net.thucydides.core.annotations.Step;
+
 public class TestSteps {
 
+    @Step("Step 1")
     public void doSomething() {
         System.out.println("Step 1");
     }
 
+    @Step("Step 2")
     public void doSomethingElse() {
         System.out.println("Step 2");
     }
